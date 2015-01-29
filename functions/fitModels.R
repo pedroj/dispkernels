1f1a5bafa703542a88059e2283726e18ae1e154e
Pedro Jordano <jordano@ebd.csic.es>
2015-01-30 00:02:32 +0100

    Summary fitting printed.

--- functions/fitModels.R
+++ functions/fitModels.R
@@ -1,11 +1,26 @@
-fitmodels <- function
-    (ff)
+fitmodels <- function (x, funcs)
+# Usage:
+# funcs<- cbind("exponential","weibull","gamma")
+# fitmodels(disp, funcs)
 {
         n <- length(funcs)
-	    funcs<-cbind("exponential", "weibull", "gamma", "pearson","lognormal", "half-normal", "half-cauchy", "half-t"); i=0
+# 	    funcs<-cbind("exponential", 
+#                      "weibull", 
+#                      "gamma", 
+#                      "pearson",
+#                      "lognormal", 
+#                 #    "half-normal",
+#                      "half-cauchy", 
+#                      "half-t"); 
+    i=0; zzz=NULL
 for (i in 1:n) {
-	            cat("\n","##### Model fitted:  ")
-	            cat(funcs[,i],"\n")
-                fit.disp(wpop.dist+1,fun=funcs[,i])
+                zz<-fit.disp(x+1,fun=funcs[,i])
+    zzz<- append(zzz, paste("\n","##### Model fitted:  ","\n"))
+    zzz<- append(zzz,paste(funcs[,i],"\n"))
+    zzz<- append(zzz,paste("dev= ", zz$dev,"\n",
+                    "AIC= ", zz$aic,"\n",
+                    "estimate= ", zz$estimate,"\n",
+                    "se= ", zz$se,"\n","\n"))
             }
+    cat(zzz)
 }
